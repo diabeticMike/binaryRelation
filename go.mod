9e09b6a3b1a662625648f0cb024b553d95771162
diabeticMike <tmymichael@gmail.com>
2020-12-09 13:02:15 +0200

    lab6 done

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/binaryRelation
 go 1.15
 
 require (
-	github.com/fogleman/gg v1.3.0
-	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/llgcode/draw2d v0.0.0-20200603164053-19660b984a28 // indirect
-	golang.org/x/image v0.0.0-20180708004352-c73c2afc3b81
+	golang.org/x/image v0.0.0-20200618115811-c13761719519 // indirect
+	gonum.org/v1/plot v0.8.1
 )
--- main.go
+++ main.go
@@ -1,541 +1,68 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"math"
-	"math/rand"
-	"time"
-
-	"github.com/fogleman/gg"
-	"github.com/golang/freetype/truetype"
-	"golang.org/x/image/font/gofont/goregular"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
+	"gonum.org/v1/plot/vg"
 )
 
-var w, h int = 1024, 1024
+// l1,l2 - коефіцієнти пропорційності швидкості зростання зброї до кількості зброї у супротивника
+// b1,b2 - коефіцієнти, що характеризують швидкість старіння власної зброї,
+// y1,y2 - функції, що описують рівень взаємної недовіри конкурентів, який вважається незалежним від кількості озброєнь
+// m10,m20 - об'єми озброєнь
+func armRace(l1, l2, b1, b2, y1, y2, m10, m20, t, n float64) ([]float64, []float64) {
+	k := int(n / t)
+	m1 := []float64{}
+	m2 := []float64{}
 
-func reflecsive(r [5][5]int) bool {
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
-			if i == j {
-
-			}
+	for i := 0; i < k; i++ {
+		if i == 0 {
+			m1 = append(m1, m10)
+			m2 = append(m2, m20)
+		} else {
+			m1 = append(m1, (t*(l1*m2[i-1]+y1)+m1[i-1])/(1-t*b1))
+			m2 = append(m2, (t*(l2*m1[i-1]+y2)+m2[i-1])/(1-t*b2))
 		}
 	}
-	return true
-}
-
-func antyreflecsive(r [5][5]int) bool {
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
-			if i == j {
-				if r[i][j] != 0 {
-					return false
-				}
-			}
-		}
-	}
-	return true
-}
-
-func symmetrical(r [5][5]int) bool {
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
-			if r[i][j] != r[j][i] {
-				return false
-			}
-		}
-	}
-	return true
-}
-
-func asymmetrical(r [5][5]int) bool {
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
-
-			if r[i][j] == 1 && r[j][i] == 1 {
-				return false
-			}
-		}
-	}
-	return true
-}
-
-func antysymmetrical(r [5][5]int) bool {
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
-			if i != j {
-				if r[i][j] == 1 && r[j][i] == 1 {
-					return false
-				}
-			}
-		}
-	}
-	return true
-}
-
-func transit(r1, r2 [5][5]int) bool {
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
-			if r1[i][j] == 1 {
-				if r1[i][j] != r2[i][j] {
-					return false
-				}
-			}
-		}
-	}
-	return true
-}
-
-func acyclic(r1, r2 [5][5]int) bool {
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
-			if r1[i][j] == 1 {
-				if r1[i][j] == r2[i][j] {
-					return false
-				}
-			}
-		}
-	}
-	return true
-}
-
-func connected(r [5][5]int) bool {
-	for i := 0; i < 5; i++ {
-		flag := false
-		for j := 0; j < 5; j++ {
-			if r[j][i] == 1 {
-				flag = true
-				break
-			}
-		}
-		if !flag {
-			return false
-		}
-	}
-	return true
-}
-
-// Maximum
-func findMax(r [5][5]int) []int {
-	res := []int{}
-	for i := 0; i < 5; i++ {
-		if r[i][0] == 1 && r[i][1] == 1 && r[i][2] == 1 && r[i][3] == 1 && r[i][4] == 1 {
-			res = append(res, i)
-		}
-	}
-
-	if len(res) == 0 {
-		return nil
-	}
-	return res
-}
-
-// Minimum
-func findMin(r [5][5]int) []int {
-	res := []int{}
-	for i := 0; i < 5; i++ {
-		if r[0][i] == 1 && r[1][i] == 1 && r[2][i] == 1 && r[3][i] == 1 && r[4][i] == 1 {
-			res = append(res, i)
-		}
-	}
-
-	if len(res) == 0 {
-		return nil
-	}
-	return res
-}
-
-// Minoranta
-func findMinor(r [5][5]int) []int {
-	res := []int{}
-	for i := 0; i < 5; i++ {
-		if r[i] == [5]int{0, 0, 0, 0, 0} {
-			res = append(res, i)
-		}
-	}
-
-	if len(res) == 0 {
-		return nil
-	}
-	return res
-}
-
-// Majoranta
-func findMajor(r [5][5]int) []int {
-	res := []int{}
-	for i := 0; i < 5; i++ {
-		if r[0][i] == 0 && r[1][i] == 0 && r[2][i] == 0 && r[3][i] == 0 && r[4][i] == 0 {
-			res = append(res, i)
-		}
-	}
-
-	if len(res) == 0 {
-		return nil
-	}
-	return res
+	return m1, m2
 }
 
 func main() {
-	r := [5][5]int{
-		{1, 1, 1, 1, 1},
-		{1, 0, 1, 1, 0},
-		{1, 1, 0, 1, 1},
-		{1, 1, 1, 0, 1},
-		{1, 0, 1, 1, 0}}
-	draw(r, "r")
 
-	fmt.Print("Відношення еквівалентності ")
-	fmt.Println(reflecsive(r) && symmetrical(r) && transit(comp(r, r), r))
+	M1, M2 := armRace(0.75, 2.2, 0.3, 0.16, 0.02, 1.2, 233, 340, 0.5, 5)
 
-	fmt.Print("Відношення нестрогого порядку ")
-	fmt.Println(reflecsive(r) && asymmetrical(r) && transit(comp(r, r), r))
-
-	fmt.Print("Відношення строгого порядку ")
-	fmt.Println(reflecsive(r) && asymmetrical(r) && transit(comp(r, r), r))
-
-	fmt.Print("Відношення лінійного порядку ")
-	fmt.Println(reflecsive(r) && antysymmetrical(r) && transit(comp(r, r), r))
-
-	fmt.Print("Відношення домінування ")
-	fmt.Println(antyreflecsive(r) && asymmetrical(r))
-
-	fmt.Print("Відношення подібності ")
-	fmt.Println(reflecsive(r) && symmetrical(r))
-
-	fmt.Println("Відношення еквівалентності ")
-	c := comp(r, ober(r))
-	for _, a := range c {
-		for _, v := range a {
-			fmt.Print(v, " ")
-		}
-		fmt.Println()
-	}
-	draw(c, "equivalent")
-	fmt.Print("Відношення еквівалентності ")
-	fmt.Println(reflecsive(c) && symmetrical(c) && transit(comp(c, c), c))
-
-	fmt.Println("Відношення толерантності ")
-	c = ob(ob(r, ober(r)), per(r, ober(r)))
-	for _, a := range c {
-		for _, v := range a {
-			fmt.Print(v, " ")
-		}
-		fmt.Println()
-	}
-	draw(c, "tolerant")
-	fmt.Print("Відношення толерантності ")
-	fmt.Println(reflecsive(c) && symmetrical(c))
-
-	fmt.Println("Відношення строгої переваги ")
-	c = riz(r, ober(r))
-	for _, a := range c {
-		for _, v := range a {
-			fmt.Print(v, " ")
-		}
-		fmt.Println()
-	}
-	draw(c, "strong_advandage")
-	fmt.Print("Відношення строгої переваги ")
-	fmt.Println(antyreflecsive(c) && symmetrical(c))
-
-	fmt.Print("Максимум ")
-	if m := findMax(r); m != nil {
-		fmt.Println(m)
-	} else {
-		fmt.Println("відсутній")
-	}
-
-	fmt.Print("Мінімум ")
-	if m := findMin(r); m != nil {
-		fmt.Println(m)
-	} else {
-		fmt.Println("відсутній")
-	}
-
-	fmt.Print("Мажоранта ")
-	if m := findMajor(r); m != nil {
-		fmt.Println(m)
-	} else {
-		fmt.Println("відсутня")
-	}
-
-	fmt.Print("Міноранта ")
-	if m := findMinor(r); m != nil {
-		fmt.Println(m)
-	} else {
-		fmt.Println("відсутня")
-	}
-
-	draw(r, "r")
-}
-
-func draw(r [5][5]int, filename string) {
-	dc := gg.NewContext(w+100, h+100)
-	dc.DrawRectangle(0, 0, float64(w+100), float64(h+100))
-	dc.SetRGB(102, 0, 102)
-	dc.Fill()
-
-	font, err := truetype.Parse(goregular.TTF)
+	p, err := plot.New()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	face := truetype.NewFace(font, &truetype.Options{Size: 48})
-	dc.SetFontFace(face)
+	m1 := make(plotter.XYs, len(M1))
+	m2 := make(plotter.XYs, len(M2))
 
-	drawNodes(r, dc)
-	dc.SavePNG(fmt.Sprint(filename, ".png"))
-}
-
-type node struct {
-	x, y float64
-}
-
-func drawNodes(r [5][5]int, dc *gg.Context) {
-	nodes := make([]node, 0, 5)
-	rand.Seed(time.Now().UnixNano())
-	for len(nodes) < 5 {
-		for {
-			x, y := float64(rand.Intn(w-200)+100), float64(rand.Intn(h-200)+100)
-			if check(nodes, x, y) {
-				nodes = append(nodes, node{x, y})
-				break
-			}
-		}
+	for i := 0; i < len(M1); i++ {
+		m1[i].X = float64(i)
+		m1[i].Y = M1[i]
 	}
 
-	drawDirections(r, nodes, dc)
-
-	for k, v := range nodes {
-		if r[k][k] != 1 {
-			dc.DrawCircle(v.x, v.y, 50)
-			dc.SetRGB255(0, 0, 0)
-			dc.Fill()
-		} else {
-			dc.DrawCircle(v.x, v.y, 50)
-			dc.SetRGB255(7, 45, 239)
-			dc.Fill()
-		}
-
-		dc.SetRGB(1, 1, 1)
-		dc.DrawStringAnchored(fmt.Sprint(k), v.x, v.y, 0.5, 0.5)
-		dc.Fill()
-
-	}
-}
-
-func check(nodes []node, x, y float64) bool {
-	for _, a := range nodes {
-		if math.Abs(a.x-x) < float64(100) {
-			return false
-		} else if math.Abs(a.y-y) < float64(100) {
-			return false
-		} else if x < 50.0 || y < 50.0 {
-			return false
-		}
-	}
-	return true
-}
-
-func drawDirections(r [5][5]int, nodes []node, dc *gg.Context) {
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
-			if r[j][i] == 1 && i != j {
-				dc.SetRGBA(226, 106, 106, 1)
-				dc.SetLineWidth(5)
-				dc.DrawLine(nodes[i].x, nodes[i].y, nodes[j].x, nodes[j].y)
-				dc.Stroke()
-				x := (nodes[i].x + nodes[j].x) / 2
-				y := (nodes[i].y + nodes[j].y) / 2
-
-				x = (x + nodes[j].x) / 2
-				y = (y + nodes[j].y) / 2
-
-				x = (x + nodes[j].x) / 2
-				y = (y + nodes[j].y) / 2
-
-				dc.SetRGBA(226, 106, 106, 1)
-				dc.SetLineWidth(15)
-				dc.DrawLine(x, y, nodes[j].x, nodes[j].y)
-				dc.Stroke()
-			}
-		}
-	}
-}
-
-func dop(r [5][5]int) [5][5]int {
-	// fmt.Println("Доповнення (r1,r2)")
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
-			if r[i][j] == 1 {
-				r[i][j] = 0
-			} else {
-				r[i][j] = 1
-			}
-		}
+	for i := 0; i < len(M2); i++ {
+		m2[i].X = float64(i)
+		m2[i].Y = M2[i]
 	}
 
-	// for _, a := range r1 {
-	// 	for _, v := range a {
-	// 		fmt.Print(v, " ")
-	// 	}
-	// 	fmt.Println()
-	// }
-	return r
-}
+	p.Title.Text = "Arm race"
+	p.X.Label.Text = "X"
+	p.Y.Label.Text = "Y"
 
-func per(r1, r2 [5][5]int) [5][5]int {
-	// fmt.Println("Перетин")
-
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
-			if r1[i][j] == r2[i][j] {
-			} else {
-				r1[i][j] = 0
-			}
-		}
+	err = plotutil.AddLinePoints(p,
+		"m1", m1,
+		"m2", m2)
+	if err != nil {
+		panic(err)
 	}
 
-	// fmt.Println()
-
-	// for _, a := range r1 {
-	// 	for _, v := range a {
-	// 		fmt.Print(v, " ")
-	// 	}
-	// 	fmt.Println()
-	// }
-	return r1
-}
-
-func ob(r1, r2 [5][5]int) [5][5]int {
-	// fmt.Println("Об'єднання")
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
-			if r1[i][j] == 1 || r2[i][j] == 1 {
-				r1[i][j] = 1
-			}
-		}
+	// Save the plot to a PNG file.
+	if err := p.Save(4*vg.Inch, 4*vg.Inch, "arm_race.png"); err != nil {
+		panic(err)
 	}
-
-	// fmt.Println()
-
-	// for _, a := range r1 {
-	// 	for _, v := range a {
-	// 		fmt.Print(v, " ")
-	// 	}
-	// 	fmt.Println()
-	// }
-	return r1
-}
-
-func riz(r1, r2 [5][5]int) [5][5]int {
-	// fmt.Println("\nРізниця")
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
-			if r2[i][j] == 1 {
-				r1[i][j] = 0
-			}
-		}
-	}
-
-	// fmt.Println()
-
-	// for _, a := range r1 {
-	// 	for _, v := range a {
-	// 		fmt.Print(v, " ")
-	// 	}
-	// 	fmt.Println()
-	// }
-	return r1
-}
-
-func ober(r1 [5][5]int) [5][5]int {
-	// fmt.Println("\nОбернена")
-
-	// fmt.Println("r1:")
-
-	c := [5][5]int{}
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
-			c[i][j] = r1[j][i]
-		}
-	}
-	// for _, a := range c {
-	// 	for _, v := range a {
-	// 		fmt.Print(v, " ")
-	// 	}
-	// 	fmt.Println()
-	// }
-
-	draw(c, "ober_r1")
-	return c
-}
-
-func zvyg(r1, r2 [5][5]int) {
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 3; j++ {
-			r1[i][j] = 0
-		}
-	}
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 5; j++ {
-			r1[i][j] = 0
-		}
-	}
-	draw(r1, "zvygennya_r1")
-
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 3; j++ {
-			r2[i][j] = 0
-		}
-	}
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 5; j++ {
-			r2[i][j] = 0
-		}
-	}
-	draw(r2, "zvygennya_r2")
-	// fmt.Println("\n звуження r1")
-
-	// for _, b := range r1 {
-	// 	for _, v := range b {
-	// 		fmt.Print(v, " ")
-	// 	}
-	// 	fmt.Println()
-	// }
-
-	// fmt.Println("\n звуження r2")
-	// for _, b := range r2 {
-	// 	for _, v := range b {
-	// 		fmt.Print(v, " ")
-	// 	}
-	// 	fmt.Println()
-	// }
-}
-
-func comp(r1, r2 [5][5]int) [5][5]int {
-	// fmt.Println("\nКомпозиція")
-
-	a := [5][5]int{}
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
-			a[i][j] = 0
-			for k := 0; k < 5; k++ {
-				a[i][j] += r1[i][k] * r2[k][j]
-				if a[i][j] > 1 {
-					a[i][j] = 1
-				}
-			}
-		}
-	}
-
-	// for _, b := range a {
-	// 	for _, v := range b {
-	// 		fmt.Print(v, " ")
-	// 	}
-	// 	fmt.Println()
-	// }
-	// draw(a, "compoz")
-	return a
 }
